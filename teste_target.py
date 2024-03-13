e95fbe9a605c581b22efacb7ebafc56d03c8b766
William Ferreira <williamroberto.crow@gmail.com>
2024-03-13 14:08:50 -0300

    feat - add new answer for question number 5

--- teste_target.py
+++ teste_target.py
@@ -17,24 +17,31 @@
 
 
 # Adição em uma lista toda a sequência de fibonacci onde é realizado os incrementos
-def fibonacci(n):
-    fib = [0, 1]
-    while fib[-1] <= n:
-        fib.append(fib[-1] + fib[-2])
-    return fib
+# def fibonacci(n):
+#     fib = [0, 1]
+#     while fib[-1] <= n:
+#         fib.append(fib[-1] + fib[-2])
+#     return fib
 
 
-# checagem se o número existe ou não na sequência
-def fib_check(num):
-    fib_seq = fibonacci(num)
-    if num in fib_seq:
-        return f"O número {num} pertence à sequência de Fibonacci."
-    else:
-        return f"O número {num} não pertence à sequência de Fibonacci."
+# # checagem se o número existe ou não na sequência
+# def fib_check(num):
+#     fib_seq = fibonacci(num)
+#     if num in fib_seq:
+#         return f"O número {num} pertence à sequência de Fibonacci."
+#     else:
+#         return f"O número {num} não pertence à sequência de Fibonacci."
 
 
-# passagem de parametros
-numero = int(input("Digite um número: "))
-print(fib_check(numero))
+# # passagem de parametros
+# numero = int(input("Digite um número: "))
+# print(fib_check(numero))
 
 # --------------------------------------------------------------------
+
+
+# 5
+# recebe a informação e inverte a string
+str = input("escreva uma palavra: ")
+str_invertida = str[::-1]
+print(str_invertida)
